3cc75298c828391f94c445669f262096c992d14e
srinivass2000 <anishtalauliker@gmail.com>
2021-03-14 13:59:12 +0530

    home route

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('home.html')
+    return render_template('investment.html')
 
 @app.route('/register')
 def register():
